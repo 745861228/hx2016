45cc633c8ae6c158a70fba184f09e93e6520b9db
往事如烟 <745861228@qq.com>
2016-12-22 19:47:10 +0800

    init application

--- app/src/main/java/com/bawei/hx2016/MainActivity.java
+++ app/src/main/java/com/bawei/hx2016/MainActivity.java
@@ -2,12 +2,30 @@ package com.bawei.hx2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.bawei.hx2016.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    private EditText nameEt;
+    private EditText passwordEt;
+    private Button commitBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        nameEt = (EditText) findViewById(R.id.name_et);
+        passwordEt = (EditText) findViewById(R.id.password_et);
+        commitBut = (Button) findViewById(R.id.commit_but);
     }
 }
---
+++ app/src/main/java/com/bawei/hx2016/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.bawei.hx2016.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.bawei.hx2016.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+}
