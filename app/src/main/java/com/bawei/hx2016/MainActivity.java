884ad0aa10aaa4682bf009c683371d65b1581cb0
往事如烟 <745861228@qq.com>
2016-12-23 13:05:55 +0800

    2016.12.23

--- app/src/main/java/com/bawei/hx2016/MainActivity.java
+++ app/src/main/java/com/bawei/hx2016/MainActivity.java
@@ -2,17 +2,20 @@ package com.bawei.hx2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.bawei.hx2016.base.BaseActivity;
+import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.exceptions.HyphenateException;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
+    private static final String TAG = "MainActivity";
     private EditText nameEt;
     private EditText passwordEt;
     private Button commitBut;
@@ -46,15 +49,46 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             //注册按钮监听
             case R.id.commit_but:
                 //注册失败会抛出HyphenateException
-                try {
-                    EMClient.getInstance().createAccount(nameEt.getText().toString().trim(), passwordEt.getText().toString().trim());//同步方法
-                } catch (HyphenateException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "注册失败！！", Toast.LENGTH_SHORT).show();
-                }
+                new Thread(){
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            EMClient.getInstance().createAccount(nameEt.getText().toString().trim(), passwordEt.getText().toString().trim());//同步方法
+                            Log.i(TAG, "run: "+"注册成功！！！");
+                        } catch (HyphenateException e) {
+                            e.printStackTrace();
+                            Log.i(TAG, "run: "+"注册失败！"+e.getErrorCode()+","+e.getMessage());
+                        }
+                    }
+                }.start();
                 break;
             //登陆按钮监听
             case R.id.login_but:
+                EMClient.getInstance().login(nameEt.getText().toString().trim(),passwordEt.getText().toString().trim(),new EMCallBack() {//回调
+                    @Override
+                    public void onSuccess() {
+                        EMClient.getInstance().groupManager().loadAllGroups();
+                        EMClient.getInstance().chatManager().loadAllConversations();
+                        Log.d("main", "登录聊天服务器成功！");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                MainActivity.this.enterActivity(ChatActivity.class);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onProgress(int progress, String status) {
+
+                    }
+
+                    @Override
+                    public void onError(int code, String message) {
+                        Log.d("main", "登录聊天服务器失败！");
+                    }
+                });
 
                 break;
         }
--- app/src/main/java/com/bawei/hx2016/base/BaseActivity.java
+++ app/src/main/java/com/bawei/hx2016/base/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.bawei.hx2016.base;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -12,5 +13,9 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    protected void enterActivity(Class activity){
+        startActivity(new Intent(this,activity));
     }
 }
