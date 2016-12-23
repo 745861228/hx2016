bc205823f96672d12a9de2ca5cba4a8e1a67a377
xichen <xichen0828@qq.com>
2016-12-23 18:23:07 +0800

    不通知

--- app/src/main/java/com/bawei/hx2016/services/MyService.java
+++ app/src/main/java/com/bawei/hx2016/services/MyService.java
@@ -9,33 +9,31 @@ import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 
 import com.bawei.hx2016.ChatActivity;
+import com.bawei.hx2016.R;
 import com.hyphenate.EMContactListener;
 import com.hyphenate.chat.EMClient;
 
 public class MyService extends Service {
     private void sendNotification(String username, String message, Class activity) {
-        Intent intent = new Intent(this, activity);
-        intent.putExtra("userName", username);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        //得到通知管理类
+        NotificationManager notificationManager = (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
         //创建一个新通知
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
         //设置builder
         builder.setTicker("message");
-        builder.setContentInfo(message);
         builder.setContentText(message);
         builder.setAutoCancel(true);
         builder.setWhen(System.currentTimeMillis());
-        builder.setDefaults(Notification.DEFAULT_VIBRATE);
+        builder.setDefaults(Notification.DEFAULT_ALL);
+
+        Intent intent = new Intent(this, activity);
+        intent.putExtra("userName", username);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         builder.setContentIntent(pendingIntent);
 
-
-        Notification notification = builder.build();
-
-
-        //得到通知管理类
-        NotificationManager notificationManager = (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
         //发送通知
-        notificationManager.notify(653, notification);
+        notificationManager.notify(653, builder.build());
     }
 
 
